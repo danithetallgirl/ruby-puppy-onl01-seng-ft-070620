c0d4261726158263848d315e97c63a02865730a2
Dani Bassett <dani@danithetallgirl.com>
2020-07-16 01:37:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,4 +6,13 @@ class Dog
   def initialize(name)
     @name = name
     @@all << self
+  end
+    
+  def self.all
+    puts @@all.collect { |dog| dog.name }
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
 end
